c17114651c621ac29f70530608bc4823074fdcec
Yaraslau Stsiazhkin <stsiazhkin@gmail.com>
2025-04-12 15:44:09 +0000

    Added CODESPACE_API_SUFFIX to settings.py for Codespace compatibility

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Add the Codespace Django REST API endpoint suffix
+CODESPACE_API_SUFFIX = "-8000.app.github.dev"
